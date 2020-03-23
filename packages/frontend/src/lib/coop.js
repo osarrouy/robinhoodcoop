a24f048bf9ec74360eecc0508f1b3033a62dc211
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-03-23 13:58:56 +0100

    Fix network mismatch bug

--- packages/frontend/src/lib/coop.js
+++ packages/frontend/src/lib/coop.js
@@ -5,8 +5,8 @@ const ADDRESS = '0x2cee3f3c8b09d8e75137936cfb9fb8f21f3654df'
 
 export const RHC = {
   new: () => {
-    const provider = typeof window.ethereum !== 'undefined' ? new ethers.providers.Web3Provider(web3.currentProvider) : ethers.getDefaultProvider('kovan')
-    const coop = typeof window.ethereum !== 'undefined' ? new ethers.Contract(ADDRESS, ABI, provider.getSigner()) : new ethers.Contract(ADDRESS, ABI, provider)
+    const provider = typeof window.ethereum !== 'undefined' && window.ethereum.networkVersion === '42' ? new ethers.providers.Web3Provider(web3.currentProvider) : ethers.getDefaultProvider('kovan')
+    const coop = typeof window.ethereum !== 'undefined' && window.ethereum.networkVersion === '42' ? new ethers.Contract(ADDRESS, ABI, provider.getSigner()) : new ethers.Contract(ADDRESS, ABI, provider)
     return coop
   },
 }
--- packages/frontend/src/lib/share.js
+++ packages/frontend/src/lib/share.js
@@ -5,8 +5,8 @@ const ADDRESS = '0x5A09A48a3c5648930604F7ea6248470b44d3b45A'
 
 export const RHS = {
   new: () => {
-    const provider = typeof window.ethereum !== 'undefined' ? new ethers.providers.Web3Provider(web3.currentProvider) : ethers.getDefaultProvider('kovan')
-    const share = typeof window.ethereum !== 'undefined' ? new ethers.Contract(ADDRESS, ABI, provider.getSigner()) : new ethers.Contract(ADDRESS, ABI, provider)
+    const provider = typeof window.ethereum !== 'undefined' && window.ethereum.networkVersion === '42' ? new ethers.providers.Web3Provider(web3.currentProvider) : ethers.getDefaultProvider('kovan')
+    const share = typeof window.ethereum !== 'undefined' && window.ethereum.networkVersion === '42' ? new ethers.Contract(ADDRESS, ABI, provider.getSigner()) : new ethers.Contract(ADDRESS, ABI, provider)
     return share
   },
 }
