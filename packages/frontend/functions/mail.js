ad3859aabb0da0097b2263afe907d539489c4aeb
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-05-25 16:51:18 +0200

    Add mail function

--- packages/frontend/functions/mail.js
+++ packages/frontend/functions/mail.js
@@ -1,21 +1,46 @@
 const mailgun = require("mailgun-js")
-const DOMAIN  = 'sandbox8084e534582a480e95019412fa442b32.mailgun.org'
+const DOMAIN  = 'sandbox3061015a63154323aad5ea2c4344af40.mailgun.org'
 const KEY     = process.env.MG_KEY
 const mg      = mailgun({apiKey: KEY, domain: DOMAIN})
 
 
-console.log(KEY)
+// console.log(KEY)
 
-// const data = {
-//   from: 'Excited User <me@samples.mailgun.org>',
-//   to:   'olivier.sarrouy@gmail.com',
-//   subject: 'Hello',
-//   text: 'Testing some Mailgun awesomness!'
-// }
 
-// mg.messages().send(data, function (error, body) {
-//     console.log('body')
-//     console.log(body)
-//     console.log('error')
-//     console.log(error)
-// })
+
+
+
+exports.handler = async event => {
+  // const subject = event.queryStringParameters.name || 'World'
+  const data = {
+    from: 'Excited User <olivier.sarrouy@gmail.com>',
+    to:   'olivier.sarrouy@gmail.com',
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomness!'
+  }
+
+  mg.messages().send(data, function (error, body) {
+
+
+
+    console.log('body')
+    console.log(body)
+    console.log('error')
+    console.log(error)
+
+    if (error) {
+      return {
+        statusCode: 500,
+        body: error.toString(),
+      }
+    } else {
+      return {
+        statusCode: 200,
+        body: 'email sen t',
+      }
+    }
+
+  })
+
+
+}
