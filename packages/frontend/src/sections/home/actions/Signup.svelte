cae54358b7a310324b7f994d165ff25367d9e6de
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-05-26 14:51:30 +0200

    Update to email-js

--- packages/frontend/src/sections/home/actions/Signup.svelte
+++ packages/frontend/src/sections/home/actions/Signup.svelte
@@ -4,6 +4,7 @@
   import { member }                from '/stores/member.js'
   import { getContext }            from 'svelte'
   import Terms                     from './Terms.md'
+  import emailjs                   from 'emailjs-com'
 
   let email     = ''
   let address   = ''
@@ -13,8 +14,13 @@
   let message   = ''
   let checked   = false
   let user      = null
+  let confirmed = false
 
-  const { open } = getContext('simple-modal')
+
+  const TEMPLATE_ID = 'template_5hkdMlok'
+  const SERVICE_ID  = 'robinhoodcoop'
+  const { open }    = getContext('simple-modal')
+  emailjs.init('user_jzjfr1GQsGlkjbK6QZpqg')
 
   const validate = () => {
     if (!firstname || firstname.length < 1) {
@@ -40,6 +46,7 @@
     message = ''
     loading = true
 
+
     if(validate()) {
       try {
         await Member.logout()
@@ -47,21 +54,14 @@
         if (await Member.isMember()) {
           message = 'You are already signed up with this address. Go back and login.'
         } else {
-        //   var myInit = { method: 'GET',
-        //        headers: myHeaders,
-        //        mode: 'cors',
-        //        cache: 'default' };
-
-        // fetch('flowers.jpg',myInit)
-        // .then(function(response) {
-        //   return response.blob();
-        // })
-          const url = 'https://keen-turing-a8352a.netlify.app/.netlify/functions/mail?firstname=' + firstname + '&lastname=' + lastname + '&email=' + email
-
-          const response = await fetch(url, { method: 'GET' })
-          console.log('Response')
-          console.log(response)
-          // user = { address: $member.address, firstname, lastname, email: $member.email }
+          console.log($member)
+          await emailjs.send(SERVICE_ID, TEMPLATE_ID, {
+            name: firstname + ' ' + lastname,
+            email: email,
+            address: address,
+            ethereum: $member.address
+          })
+          confirmed = true
         }
       } catch (e) {
         message = e.message
@@ -76,21 +76,21 @@
 </script>
 
 <section class="flex column">
-  {#if user}
+  {#if confirmed}
     <Animate>
       <p class="small">
-        1. Request membership by email
-        <Mail {user}>here</Mail>
+        Your membership request has been submitted to review by an admin.
       </p>
-      <p class="small">2. Do not modify the subject nor the beginning of the email</p>
-      <p class="small">3. Send the email from the same address you just registered with</p>
+      <p class="small">
+        We will come back to you soon.
+      </p>
     </Animate>
   {:else}
     <Animate>
-      <p class="small">Your membership request will be reviewed by an admin. Please:</p>
-      <p class="small">1. use your real name</p>
-      <p class="small">1. use the same email you are identified with if you already are a RobinHood member</p>
-      <p class="small space-top">Once you click on 'signup' a Fortmatic window will pop up. This Formatic account will identify you on this platform.</p>
+      <p class="x-small">Your membership request will be reviewed by an admin. Please:</p>
+      <p class="x-small">1. use your real name</p>
+      <p class="x-small">1. use the same email you are identified with if you already are a RobinHood member</p>
+      <p class="x-small space-top">Once you click on 'signup' a Fortmatic window will pop up. This Formatic account will identify you on this platform.</p>
       <form class="space-top" on:submit|preventDefault={signup}>
         <div class="flex"><input bind:value={email} placeholder="me@mail.com" /></div>
         <div class="flex space-top"><input class="space-top" bind:value={address} placeholder="1 Rue des Dames, 75001 Paris, France" /></div>
@@ -102,10 +102,10 @@
           <input class="space-right" type="checkbox"  bind:checked={checked} />
           <p class="info">I agree with the <a href="/#" on:click|preventDefault={showTerms}>terms and conditions</a>.</p>
         </div>
+        <p class="info">{message}</p>
         <div class="flex space-top">
           <Submit disabled={loading} value="signup" />
         </div>
-        <p class="info space-top">{message}</p>
       </form>
     </Animate>
   {/if}
